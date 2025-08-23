3616da99097f316fd63ac437d72e113b44ee4e07
leedongkyu <leedongkyu0225@gmail.com>
2025-08-23 20:39:10 +0900

    feat: mutable and unmutable

---
+++ variable/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let x = 5; // const
+    println!("The value of x is : {x}");
+    let mut y = 3;
+    y = 11;
+    println!("The value of y is : {y}");
+    let y = 12;
+    let sample = y;
+    println!("{y}");
+    let t = true;
+    let f: bool = false;
+
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+}
